85760f1070ec184f84eaca584342cc8ab01bcce0
Harald Kube <harald.kube@gmx.de>
2017-07-28 01:52:45 +0200

    Do not use OpenCV to read image because it does not work on AWS

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 #!python
 
 import csv
-import cv2
+import matplotlib.image as mpimg
 import numpy as np
 import os
 from numpy import imag
@@ -23,14 +23,14 @@ steering_angles = []
 for line in csv_lines:
     orig_img_path = line[0]
     local_img_path = os.path.join(DATA_DIR, "IMG", os.path.basename(orig_img_path))
-    image = cv2.imread(local_img_path)
+    image = mpimg.imread(local_img_path)
     if img_shape is None:
         img_shape = image.shape
     images.append(image)
     angle = float(line[3])
     steering_angles.append(angle)
     #print("path:", local_img_path, "  shape:", image.shape, "  steering angle:", angle)
-    #break
+    #exit()
 
 X_train = np.array(images)
 y_train = np.array(steering_angles)
