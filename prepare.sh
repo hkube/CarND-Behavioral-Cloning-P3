0687ff5b11a58906df7c600a2848bff1d6e76b3f
Harald Kube <harald.kube@gmx.de>
2017-08-03 22:36:28 +0200

    Download data archives from dropbox

--- prepare.sh
+++ prepare.sh
@@ -9,8 +9,16 @@ git config --global --add alias.co checkout
 git config --global --add user.email "harald.kube@gmx.de"
 git config --global --add user.name  "Harald Kube"
 
-for a in data2.tar.bz2 data3.tar.bz; do
-	echo Downloading $a
-	wget http://www.hkube.net/udacity/$a
+#for a in data2 data3; do
+#	echo Downloading $a
+#	wget http://www.hkube.net/udacity/${a}.tar.bz2
+#	tar -xf $a
+#done
+
+wget https://www.dropbox.com/sh/s0mfjceut1kllh8/AADmkdmeL_GLZzZcJvTII9Tka/data1.tar.bz2?dl=0 -O data1.tar.bz2
+wget https://www.dropbox.com/sh/s0mfjceut1kllh8/AAD5DU1vYjX5ZWy2vKfSTHqUa/data2.tar.bz2?dl=0 -O data2.tar.bz2
+wget https://www.dropbox.com/sh/s0mfjceut1kllh8/AADkZqrR4xwHukkYhvAFCIKba/data3.tar.bz2?dl=0 -O data3.tar.bz2
+
+for a in *.tar.bz2: do
 	tar -xf $a
 done
